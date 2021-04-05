8a3e7223c1a207fabd705afd98d39c21a77bd7d8
aldinh777 <aldi.nh789@gmail.com>
2021-04-05 19:57:06 +0800

    Bunch a little Fixes & buff Energy Transfer

--- src/main/java/aldinh777/potatoheadshot/compat/jei/mana/CollectorRecipe.java
+++ src/main/java/aldinh777/potatoheadshot/compat/jei/mana/CollectorRecipe.java
@@ -1,9 +1,7 @@
 package aldinh777.potatoheadshot.compat.jei.mana;
 
-import aldinh777.potatoheadshot.lists.PotatoBlocks;
 import aldinh777.potatoheadshot.lists.PotatoItems;
 import com.google.common.collect.Lists;
-import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
@@ -17,20 +15,9 @@ public class CollectorRecipe extends AbstractManaRecipe {
 	
 	public static List<CollectorRecipe> getRecipes() {
 		List<CollectorRecipe> jeiRecipes = Lists.newArrayList();
-		
-		Item flower = Item.getItemFromBlock(Blocks.RED_FLOWER);
-		Item stone = Item.getItemFromBlock(Blocks.STONE);
-		Item torch = Item.getItemFromBlock(Blocks.TORCH);
-		
-		Item manaFlower = Item.getItemFromBlock(PotatoBlocks.MANA_FLOWER);
-		Item manaStone = Item.getItemFromBlock(PotatoBlocks.MANA_STONE);
-		Item manaTorch = Item.getItemFromBlock(PotatoBlocks.MANA_TORCH);
-		
+
 		addRecipe(jeiRecipes, PotatoItems.GLOWING_POTATO_DUST, PotatoItems.MANA_DUST);
-		addRecipe(jeiRecipes, flower, manaFlower);
-		addRecipe(jeiRecipes, stone, manaStone);
-		addRecipe(jeiRecipes, torch, manaTorch);
-		
+
 		return jeiRecipes;
 	}
 
