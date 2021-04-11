c3ef8bb308381b8c9e9839d8e3f50a9696c0fac6
aldinh777 <aldi.nh789@gmail.com>
2021-04-12 01:41:05 +0800

    Version 1.7.2

--- src/main/java/aldinh777/potatoheadshot/PotatoHeadshot.java
+++ src/main/java/aldinh777/potatoheadshot/PotatoHeadshot.java
@@ -13,7 +13,7 @@ import java.io.File;
 public class PotatoHeadshot {
     public static final String MODID = "potatoheadshot";
     public static final String NAME = "Potato Headshot";
-    public static final String VERSION = "1.7.1";
+    public static final String VERSION = "1.7.2";
 
     public static File CONFIG_DIR;
     public static Logger LOGGER;
